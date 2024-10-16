1fed1759eca2c2a29a023719b81572f92abc5209
ForSureNotReaper <kobra.olinek13@gmail.com>
2024-10-16 12:42:14 +0200

    Update Program.cs

--- dupka/dupka/Program.cs
+++ dupka/dupka/Program.cs
@@ -2,11 +2,23 @@
 {
     static void Main(string[] args)
     {
-
+        Zad1();
     }
     static void Zad1()
     {
-        Console.WriteLine("Podaj Liczbe");
-        int x = int.Parse(Console.ReadLine());
+        bool grabyskrecikebabywturcjidlategogoniema = true;
+        while(true)
+        {
+            Console.WriteLine("Zupa");
+            if (grabyskrecikebabywturcjidlategogoniema)
+            {
+                Console.WriteLine("Grabyskrecikebabywturcji");
+            }
+            else { 
+                Console.WriteLine("GrabyskrecikebabywIndiach");
+            }
+            Console.WriteLine("-------------------------------");
+            Thread.Sleep(1000); 
+        }
     }
 }
